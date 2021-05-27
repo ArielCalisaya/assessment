2f90553e9228d7d224630725390457aeac2c56f4
ArielCalisaya <calisayaariel62@gmail.com>
2021-05-27 13:12:55 -0400

    starting with multiselect

--- src/pages/quiz/[slug]/index.js
+++ src/pages/quiz/[slug]/index.js
@@ -6,7 +6,7 @@ import QuizCard from "src/components/quizCard";
 import Head from "next/head";
 
 export const getStaticPaths = async () => {
-  const res = await fetch("https://breathecode.herokuapp.com/v1/assessment/");
+  const res = await fetch("https://raw.githubusercontent.com/ArielCalisaya/assessment/master/src/util/example.json");
   const data = await res.json();
   const paths = data.map((res) => {
     return {
@@ -22,7 +22,7 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context) => {
   const slug = context.params.slug;
   const res = await fetch(
-    `https://breathecode.herokuapp.com/v1/assessment/${slug}`
+    `https://raw.githubusercontent.com/ArielCalisaya/assessment/master/src/util/example.json${slug}`
   );
   const data = await res.json();
   return {
